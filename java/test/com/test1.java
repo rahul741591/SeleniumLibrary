0ff4240b7b6e4e043a00d22b667e8f6f52d93c41
rahul741591 <50498041+rahul741591@users.noreply.github.com>
2019-10-05 13:02:06 +0530

    initial commit
    initial commit

---
+++ java/test/com/test1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test.com;
+
+/**
+ * @author rahul
+ *
+ */
+public class test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
